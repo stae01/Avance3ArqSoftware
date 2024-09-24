8ccf0edf3d187cbe7d627f5bf99e191663230b2a
stae01 <166432863+stae01@users.noreply.github.com>
2024-09-23 22:08:52 -0700

    a3

---
+++ Avance3ArqSoftware/src/main/java/com/mycompany/avance3arqsoftware/Avance3ArqSoftware.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.avance3arqsoftware;
+
+/**
+ *
+ * @author user
+ */
+public class Avance3ArqSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
